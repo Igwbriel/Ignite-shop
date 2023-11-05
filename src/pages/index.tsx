ab49354af9ac495b7e075b647238516ea0b9c2ed
Gabriel Dias <GabrielDias7820@gmail.com>
2023-11-05 15:59:09 -0300

    aplicando e configurando a biblioteca stitches

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import { styled } from "../styles"
+
+const Button = styled('button', {
+  backgroundColor: '$rocketseat',
+  borderRadius: 4,
+  border: 0,
+  padding: '4px 8px',
+
+  span: {
+    fontWeight: 'bold',
+  },
+
+  '&:hover': {
+    filter: 'brightness(0.8)'
+  }
+})
 
 export default function Home() {
   return (
-  <h1>Hello World</h1>
+  <Button>
+    <span> teeste </span>
+    Enviar</Button>
   )
 }
---
+++ src/styles/index.ts
@@ -0,0 +1,18 @@
+import { createStitches } from "@stitches/react";
+
+export const {
+  config,
+  theme,
+  css,
+  styled, 
+  globalCss,
+  keyframes,
+  getCssText,
+  createTheme
+} = createStitches({
+  theme: {
+    colors: {
+      rocketseat: '#8257e6'
+    }
+  }
+})
