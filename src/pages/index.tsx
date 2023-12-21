a916890972c404cdc329b0cd7479f731691c646e
Gabriel Dias <GabrielDias7820@gmail.com>
2023-12-21 15:30:07 -0300

    Formatando o preço dos produtos

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -54,7 +54,10 @@ export const getStaticProps: GetStaticProps = async () => {
       id: product.id,
       name: product.name,
       imageUrl: product.images ? product.images[0] || '' : '',
-      price: price.unit_amount / 100,
+      price: new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+      }).format(price.unit_amount / 100),
     }
   })
 
