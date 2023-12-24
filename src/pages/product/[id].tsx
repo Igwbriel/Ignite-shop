5e68e5f9d596adf841ea84b608bfc199570017f0
Gabriel Dias <GabrielDias7820@gmail.com>
2023-12-23 22:45:02 -0300

    Estrutura da página do produto

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,7 +1,30 @@
 import { useRouter } from 'next/router'
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from '../../styles/pages/product'
 
 export default function Product() {
   const { query } = useRouter()
 
-  return <h1>Produto: {JSON.stringify(query)}</h1>
+  return (
+    <ProductContainer>
+      <ImageContainer></ImageContainer>
+
+      <ProductDetails>
+        <h1> Camiseta X</h1>
+        <span>R$ 79,90</span>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta amet
+          dolorem rerum incidunt asperiores qui? Tempora amet ipsa perspiciatis
+          quaerat at, quam explicabo quas excepturi temporibus inventore minus?
+          Dicta, odio?
+        </p>
+
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
+  )
 }
